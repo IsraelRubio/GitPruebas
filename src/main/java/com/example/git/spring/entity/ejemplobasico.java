2f1e680b646d625fbb8016c027fc59ab7657318f
IsraelRubio <rubioontanilla@gmail.com>
2020-10-04 13:02:29 +0200

    cambio desde git para conflicto pull desde git

--- src/main/java/com/example/git/spring/entity/ejemplobasico.java
+++ src/main/java/com/example/git/spring/entity/ejemplobasico.java
@@ -6,4 +6,5 @@ public class ejemplobasico {
 	private int cambio_gitBajado;
 	private int cambio_desdeGit;
 	private int cambio_paraGitConflicto_abajo;
+	private int cambio_paraConflictoPull_desdeGit;
 }
